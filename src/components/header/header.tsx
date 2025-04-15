c476b3b3fa344d4008f54ec49f66cf36413ea6c3
Jean Andrade <jeancarlos.av0@gmail.com>
2025-04-14 21:37:39 -0300

    style(header): Foi realizado uma mudança geral na estrutura do header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 
 export function Header() {
   return (
-    <div className="max-w-[1216px] mx-auto py-5 flex-col gap-5">
-      <div className="bg-zinc-50 opacity-90 w-auto h-20 flex justify-between items-center m-6 px-4 rounded-full">
+    <div className="max-w-[1740px] mx-auto py-5 flex-col gap-5">
+      <div className="opacity-90 w-auto h-20 flex justify-between items-center m-6 px-4 rounded-full">
         <ul>
           <Link href={"/"}><Image src="/logo.png" width={50} height={50} alt="Logo Materium" /></Link>
         </ul>
 
-        <ul className="bg-white flex gap-9 text-center p-5 items-center rounded-full">
+        <ul className="bg-zinc-100 opacity-85 flex gap-9 text-center text-zinc-700 font-semibold px-6 py-2 items-center rounded-full">
           <li><Link href="/produtos">Produtos</Link></li>
           <li><Link href="/solucoes">Soluções</Link></li>
           <li><Link href="/precos">Preços</Link></li>
@@ -18,9 +18,9 @@ export function Header() {
         </ul>
 
         <ul className="flex justify-center items-center gap-9">
-          <li><Link href="/login">Login</Link></li>
+          <li className="text-zinc-700 font-semibold opacity-85"><Link href="/login">Login</Link></li>
           <Link href="/cadastro">
-            <button className="border-2 border-zinc-300 px-6 py-4 rounded-full hover:bg-zinc-100 cursor-pointer">
+            <button className="border-2 border-zinc-500 bg-[#A3CAE9] text-zinc-700 opacity-85 font-semibold px-6 py-4 rounded-full hover:bg-blue-200 cursor-pointer">
               Cadastrar-se
             </button>
           </Link>
