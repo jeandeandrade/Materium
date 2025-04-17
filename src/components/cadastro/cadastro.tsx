b58a28d1b9bab3cc10c30450fa8edfee2b3d89b4
Jean Andrade <jeancarlos.av0@gmail.com>
2025-04-16 21:00:33 -0300

    style(cadastro): Adicionado o campo 'Papel' e useState para os icones dos campos senha e papel

--- src/components/cadastro/cadastro.tsx
+++ src/components/cadastro/cadastro.tsx
@@ -1,7 +1,13 @@
+"use client";
+
 import Link from "next/link";
-import { Eye } from "lucide-react";
+import { Eye, EyeOff, ArrowDown } from "lucide-react";
+import { useState } from "react";
 
 export function Cadastro() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
   return (
     <div className="flex justify-center items-center min-h-screen bg-gradient-to-br from-sky-100 via-orange-100 to-blue-100">
       <div className="bg-white max-w-md w-full rounded-3xl shadow-xl px-8 py-10">
@@ -26,7 +32,7 @@ export function Cadastro() {
           <input
             type="email"
             placeholder="alexsmith.mobbin@gmail.com"
-            className="w-full rounded-full border border-gray-300 px-4 py-2 focus:outline-none focus:ring-1 focus:ring-blue-300"
+            className="w-full rounded-md border border-gray-300 px-4 py-2 focus:outline-none focus:ring-1 focus:ring-blue-300"
           />
         </div>
 
@@ -38,7 +44,7 @@ export function Cadastro() {
             <input
               type="text"
               placeholder="Will Smith"
-              className="w-full rounded-full border border-gray-300 px-4 py-2 focus:outline-none focus:ring-1 focus:ring-blue-300"
+              className="w-full rounded-md border border-gray-300 px-4 py-2 focus:outline-none focus:ring-1 focus:ring-blue-300"
             />
           </div>
         </div>
@@ -48,19 +54,57 @@ export function Cadastro() {
             Senha
           </label>
           <input
-            type="password"
-            placeholder="**************"
-            className="w-full rounded-full border border-gray-300 px-4 py-2 pr-10 focus:outline-none focus:ring-1 focus:ring-blue-300"
+            type={isPasswordVisible ? "text" : "password"}
+            placeholder="********"
+            className="w-full rounded-md border border-gray-300 px-4 py-2 pr-10 focus:outline-none focus:ring-1 focus:ring-blue-300"
           />
-          <Eye className="absolute right-3 top-9 text-gray-500 w-5 h-5 cursor-pointer" />
+          {isPasswordVisible ? (
+            <Eye
+              className="absolute right-3 top-9 text-gray-500 w-5 h-5 cursor-pointer"
+              onClick={() => setIsPasswordVisible(false)}
+            />
+          ) : (
+            <EyeOff
+              className="absolute right-3 top-9 text-gray-500 w-5 h-5 cursor-pointer"
+              onClick={() => setIsPasswordVisible(true)}
+            />
+          )}
         </div>
 
-        <button
-          type="submit"
-          className="w-full border-2 border-zinc-300 bg-blue-300 p-3 rounded-full text-zinc-600 text-sm tracking-wide font-semibold hover:bg-blue-400 hover:cursor-pointer hover:text-zinc-700"
-        >
-          Continuar
-        </button>
+        <div className="mb-7 relative mt-[-15px]">
+          <label className="block text-sm font-medium text-zinc-700 mb-1">
+            Papel
+          </label>
+          <select
+            name="papel"
+            onFocus={() => setIsOpen(true)}
+            onBlur={() => {
+              setTimeout(() => {
+                setIsOpen(false);
+              }, 100);
+            }}
+            className="w-full rounded-md border border-gray-300 px-4 py-2 appearance-none text-zinc-700 focus:outline-none focus:ring-1 focus:ring-blue-300"
+          >
+            <option value="fornecedor">Fornecedor</option>
+            <option value="comprador">Comprador</option>
+            <option value="funcionario">Funcionário</option>
+            <option value="admin">Admin</option>
+          </select>
+          <ArrowDown
+            className={`absolute right-3 top-9 text-gray-500 w-5 h-5 transition-transform duration-300 ease-in-out ${
+              isOpen ? "rotate-90" : ""
+            }`}
+          />
+        </div>
+
+        <Link href="/">
+          <button
+            type="submit"
+            className="w-full border-2 border-zinc-300 bg-blue-300 p-3 rounded-full text-zinc-600 text-sm tracking-wide font-semibold hover:bg-blue-400 hover:cursor-pointer hover:text-zinc-700"
+          >
+            Continuar
+          </button>
+        </Link>
 
         <p className="text-xs text-center text-gray-500 mt-6 px-4">
           Ao se inscrever, você reconhece que leu, entendeu e concorda com os
